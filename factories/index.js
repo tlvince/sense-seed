b81f132d73fbb87b3ac7378f7cf3b72f70359560
Tom Vincent <git@tlvince.com>
2014-09-04 20:59:52 +0200

    Simplify factory name-path map

--- factories/index.js
+++ factories/index.js
@@ -3,31 +3,31 @@
 var Factory = require('factory-lady');
 
 var factories = [
-  { labResult: 'lab-result' },
-  { caseInvestigation: 'case-investigation' },
-  { followUp: 'follow-up' }
+  {
+    name: 'labResult',
+    path: 'lab-result'
+  },
+  {
+    name: 'caseInvestigation',
+    path: 'case-investigation'
+  },
+  {
+    name: 'followUp',
+    path: 'follow-up'
+  }
 ];
-
-var factoryNames = factories.map(function(factory) {
-  return Object.keys(factory)[0];
-});
-
-var factoryPaths = factories.map(function(factory) {
-  var name = Object.keys(factory)[0];
-  return factory[name];
-});
 
 module.exports = {
   build: function build() {
-    factoryPaths.forEach(function(factory) {
-      require('./' + factory)();
+    factories.forEach(function(factory) {
+      require('./' + factory.path)();
     });
   },
   save: function save(count) {
     count = count || 1;
-    factoryNames.forEach(function(factory) {
+    factories.forEach(function(factory) {
       for (var i = 0, len = count; i < len; i++) {
-        Factory.create(factory);
+        Factory.create(factory.name);
       }
     });
   }
